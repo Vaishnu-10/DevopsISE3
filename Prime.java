7c57c8bc4dc680305520ad8aa095b3df83a793b1
Vaishnavi Mahamuni <79653603+Vaishnu-10@users.noreply.github.com>
2023-06-09 22:11:53 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,26 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int count = 0;
+        int number = 2;
+
+        while (count < 100) {
+            if (isPrime(number)) {
+                System.out.print(number + " ");
+                count++;
+            }
+            number++;
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
